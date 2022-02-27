1c0b0edfcd3e54d5c5ce960d00d714bcf017647a
Nick Martin <martin.nicholas.r@gmail.com>
2022-02-26 21:39:36 -0700

    updating functionality... everything appears to be functioning

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+// required packages stored in shorthand variables
+const inquirer = require('inquirer');
+const fs = require('fs');
+const mdGenny = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// inquirer prompt used to collect information from user via the terminal
+const promptUser = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of your application?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'Enter a brief description:',
+                name: 'desc',
+            },
+            {
+                type: 'list',
+                message: 'Which license would you like to use?',
+                name: 'license',
+                choices: ['Apache 2.0', 
+                        'GNU_General_Public_License_v3.0', 
+                        'GNU General Public License v2.0', 
+                        'MIT', 
+                        'BSD 2-Clause "Simplified" License', 
+                        'BSD 3-Clause "New" or "Revised" License', 
+                        'Boost Software License v1.0',
+                        'Creative Commons Zero v1.0 Universal', 
+                        'Eclipse Public License 2.0', 
+                        'GNU Affero General Public License v3.0', 
+                        'GNU Lesser General Public License v2.1',
+                        'Mozilla Public License v2.0', 
+                        'The Unilicense'],
+             },
+             {
+                type: 'input',
+                message: 'How does the user install the application?',
+                name: 'install',
+             },
+             {
+                type: 'input',
+                message: 'Anything the user should know about using the application?',
+                name: 'usage',
+             },
+             {
+                type: 'input',
+                message: 'How can the user contribute to the application?',
+                name: 'contribution',
+             },
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: 'Enter the url for your github profile:',
+                name: 'profileLink',
+            },
+            {
+                type: 'input',
+                message: 'What email would you like to provide for questions about your application?',
+                name: 'email',
+            },
+        ]);
+};
+  
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+// init function that is a series of promises to collect information from user and write the collected answers to a README file
+const init = () => {
+    promptUser()
+    .then((answers) => fs.writeFileSync('README.md', mdGenny.generateMarkdown(answers)))
+    .then(() => console.log('Successfully generated README!'))
+    .catch((err) => console.error(err));
+};
+ 
+///// APPLICATION GO BRRRRRRRR -----> 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,82 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to generation Markdown based on deconstructed object passed from inquirer
+const generateMarkdown = ({name, license, desc, install, usage, contribution, username, profileLink, email}) => {
+  let licenseBadge;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Determine License Badge based on license selection from inquirer
+  switch (license) {
+    case license = 'Apache 2.0':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case license = 'GNU General Public License v3.0':
+      licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case license = 'GNU General Public License v2.0':
+      licenseBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+      break;
+    case license = 'MIT':
+      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case license = 'BSD 2-Clause "Simplified" License':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_2-Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+      break;
+    case license = 'BSD 3-Clause "New" or "Revised" License':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3-Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      break;
+    case license = 'Boost Software License v1.0':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+      break;
+    case license = 'Creative Commons Zero v1.0 Universal':
+      licenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+      break;
+    case license = 'Eclipse Public License 2.0':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+      break;
+    case license = 'GNU Affero General Public License v3.0':
+      licenseBadge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+      break;
+    case license = 'GNU Lesser General Public License v2.1':
+      licenseBadge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+      break;
+    case license = 'Mozilla Public License v2.0':
+      licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+    case license = 'The Unilicense':
+      licenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+      break;
+    default:
+      licenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+      break;
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  // Retunr the markdown template to index.js so it can be written to the generated README.md file'
+  return `# ${name}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  ${licenseBadge}
+  <br />
+  
+  ${desc}
+  <br />
 
-`;
+  ## How to Install
+  ${install}
+  <br />
+
+  ## Usage
+  ${usage}
+  <br />
+
+  ## How to Contribute
+  ${contribution}
+
+  ## Contact
+  If you have any questions, contact the creator at:
+
+  * [${username}](${profileLink})
+  * ${email}
+  `
 }
-
-module.exports = generateMarkdown;
+ 
+// Exporting function so it can be used in index.js
+module.exports = {
+  generateMarkdown
+}
